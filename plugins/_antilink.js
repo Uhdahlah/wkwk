bd5d9272016309b7a2382c0bb251d62ee85c835d
Uhdahlah <89252558+Uhdahlah@users.noreply.github.com>
2021-12-28 09:43:52 +0700

    Update _antilink.js
    Pembaruan biar bisa ngekick

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -1,15 +1,15 @@
 let linkRegex = /chat.whatsapp.com\/(?:invite\/)?([0-9A-Za-z]{20,24})/i
 module.exports = {
-  before(m, { isAdmin, isBotAdmin }) {
+  before(m, { isAdmin, isBotAdmin, isUser }) {
     if (m.isBaileys && m.fromMe) return true
     let chat = global.db.data.chats[m.chat]
     let isGroupLink = linkRegex.exec(m.text)
 
     if (chat.antiLink && isGroupLink) {
-      m.reply('Hapus!!\n\nLink Grup terdeteksi')
+      m.reply('*ANTILINK GRUB*\n\nkarena sudah mengirim link grub kamu bakal di kick')
       if (global.opts['restrict']) {
-        if (isAdmin || !isBotAdmin) return true
-        // this.groupRemove(m.chat, [m.sender])
+        if (isUser) return true
+        ,this.groupRemove(m.chat, [m.sender])
       }
     }
     return true
